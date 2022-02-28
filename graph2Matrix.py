215b07dad22b4c10b8cd9271e0bf5d55553ee7f3
Mritunjay1729 <77975156+Mritunjay1729@users.noreply.github.com>
2022-03-01 00:29:27 +0530

    Add code implementation with explanations

---
+++ graph2Matrix.py
@@ -0,0 +1,45 @@
+import networkx as nx
+import numpy as np
+import copy
+
+# graph2Matrix returns both the adjacency matrix as well
+# as laplacianMatrix for a graph G. It takes 2 inputs, G, and
+# weighted(bool) which signifies whether graph is weighted or not.
+
+# Adjacency matrix : It is a matrix A(nxn) where n = number of nodes
+# and edge between any two vertices is represented by M(i, j)
+# for normal graphs, if edge exists : M(i, j) = 1 else M(i, j) = 0
+# for weighted graphs, M(i, j) = k(weight of the edge)
+
+# lapacian Matrix: It is a matrix L(nxn) where n = number of nodes in graph
+# In this matrix, the diagonal nodes repesent the degree of i for a pair L(i, i)
+# We have, L(i, j) = degree(node i), i==j for non-weighted graph, sum of weights of all its neighbours for a weighted graph
+#                  = -1, i != j, but edge exists
+#                  = 0, no edge
+
+def graph2matrix(G, weighted=False):
+    N = len(G.nodes())
+    adjMatrix = np.zeros((N, N))
+    
+    nodes = list(G.nodes())
+    edges = list(G.edges())
+    
+    for edge in edges:
+        i = nodes.index(edge[0])
+        j = nodes.index(edge[1])
+        if not weighted:
+            adjMatrix[i][j], adjMatrix[j][i] = 1, 1
+        else:
+            adjMatrix[i][j], adjMatrix[j][i] = G.get_edge_data(edge[0], edge[1])['weight'], G.get_edge_data(edge[0], edge[1])['weight']
+            
+    laplacianMatrix = copy.deepcopy(-adjMatrix)
+    for i in range(0, N):
+        if weighted:
+            strength = 0
+            for n in G.neighbors(nodes[i]):
+                strength += G.get_edge_data(nodes[i], n)['weight']
+            laplacianMatrix[i][i] = strength
+        else:
+            laplacianMatrix[i][i] = G.degrees(nodes[i])
+            
+    return adjMatrix, laplacianMatrix
---
+++ labelPropagtionSourceDetection.py
@@ -0,0 +1,50 @@
+from os import readlink
+import graph2Matrix as g2m
+import numpy as np
+import random
+
+#Basic Label Propagation Source Detection
+def labelPropagtionSourceDetection(G, IG, alpha):
+    nodes = list(G.nodes())
+    N = len(nodes)
+    infNodes = list(IG.nodes)
+    
+    W, LL = g2m.graph2matrix(G, weighted=False)
+    D = np.zeros((N, N))
+    for i in range(0, N):
+        D[i][i] = (sum(W[i])) ** (-0.5)
+    S = np.dot(np.dot(D, W), D) #S = D(-1/2)*W*D(-1/2)
+    
+    #We can alternately use a different method altogether
+    DD = np.zeros((N, N))
+    for i in range(0, N):
+        DD[i][i] = 1/(sum(W[i]))
+    #Variants - Try out with different variants while replacing with S
+    P1 = np.dot(DD, W) #1st variant
+    P2 = P1.transpose() #Transpose
+    
+    # Converting from set of infected nodes to an array
+    # with infected nodes represented by 1 and non-infected nodes by -1 
+    MY = np.zeros(N)
+    for i in range(0, N):
+        MY[i] = -1
+    for infNode in infNodes:
+        i = nodes.index(infNode)
+        MY[i] = 1
+    MY = MY.T #Array
+    f = MY
+    
+    # Calculate till f reaches convergence
+    while(True):
+        fnew = alpha * S.dot(f) + (1- alpha) * MY
+        if np.linalg.norm(f - fnew) < 0.0001 * N:
+            break
+        f = fnew
+    
+    maxf = -10000
+    detectedSource = nodes[random.randint(0, len(nodes)-1)]
+    for i in range(0, N):
+        if f[i] > maxf and nodes[i] in infNode:
+            maxf = f[i]
+            detectedSource = nodes[i]
+    return detectedSource
